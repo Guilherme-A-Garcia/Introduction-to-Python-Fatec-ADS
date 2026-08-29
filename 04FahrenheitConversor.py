505eda214bd178b345f9cd17f8aef76be88e8c8f
Guilherme G. <guilherme.garcia.dev.br@gmail.com>
2026-08-29 01:41:45 -0300

    feat: finish the fourth assignment

---
+++ 04FahrenheitConversor.py
@@ -0,0 +1,7 @@
+celsius: float = 0
+fahrenheit: float = 0
+
+celsius = float(input("Insira a temperatura em Celsius (°C): "))
+fahrenheit = (9.0 * celsius + 160.0) / 5.0
+
+print("Temperatura em Fahrenheit:", fahrenheit,"°F")
