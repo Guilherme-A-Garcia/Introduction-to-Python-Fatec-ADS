451254eb272acc44d4a5b6467c6557568f82adde
Guilherme G. <guilherme.garcia.dev.br@gmail.com>
2026-08-29 02:31:30 -0300

    feat: finish the ninth assignment

---
+++ 09SquareSum.py
@@ -0,0 +1,10 @@
+x: int = 0
+y: int = 0
+resultado: int = 0
+
+x = int(input("Insira o valor de X: "))
+y = int(input("Insira o valor de Y: "))
+
+resultado = (y ** 2) + (x ** 2)
+
+print("Resultado:", resultado)
