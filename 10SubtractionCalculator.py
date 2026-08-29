cc77b6203af338e3360e50d1105ad0d48fc2bfbe
Guilherme G. <guilherme.garcia.dev.br@gmail.com>
2026-08-29 02:34:29 -0300

    feat: finish the tenth assignment

---
+++ 10SubtractionCalculator.py
@@ -0,0 +1,10 @@
+x: float = 0
+y: float = 0
+diferenca: float = 0
+
+x = float(input("Insira o valor de X: "))
+y = float(input("Insira o valor de Y: "))
+
+diferenca = x - y
+
+print("Diferença:", diferenca)
