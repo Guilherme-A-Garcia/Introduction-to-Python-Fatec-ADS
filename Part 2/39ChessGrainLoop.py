3de51ea8732a13f9ff0c1c962753a3d9b049f3d7
Guilherme G. <guilherme.garcia.dev.br@gmail.com>
2026-09-11 05:12:25 -0300

    feat: finish the thirty-ninth assignment

---
+++ Part 2/39ChessGrainLoop.py
@@ -0,0 +1,15 @@
+casa: int
+qtd_casa: int
+qtd_total: int
+
+qtd_total = 1
+qtd_casa = 1
+
+casa = int(input("Insira o número máximo de casas (menor que 65): "))
+while casa > 64:
+    casa = int(input("Insira o número máximo de casas (menor que 65): "))
+
+for i in range(1, casa + 1):
+    qtd_total += qtd_casa
+    qtd_casa *= 2
+print("Total de grãos:", qtd_total)
