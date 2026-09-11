f5c723717f407de69746678044062fe129dc5524
Guilherme G. <guilherme.garcia.dev.br@gmail.com>
2026-09-11 05:13:30 -0300

    fix(39ChessGainLoop.py): remove +1 from range()

--- Part 2/39ChessGrainLoop.py
+++ Part 2/39ChessGrainLoop.py
@@ -9,7 +9,7 @@ casa = int(input("Insira o número máximo de casas (menor que 65): "))
 while casa > 64:
     casa = int(input("Insira o número máximo de casas (menor que 65): "))
 
-for i in range(1, casa + 1):
+for i in range(1, casa):
     qtd_total += qtd_casa
     qtd_casa *= 2
 print("Total de grãos:", qtd_total)
