114f9e6a163760a53b1c8e5c73b57ec52740eab3
Guilherme G. <guilherme.garcia.dev.br@gmail.com>
2026-09-10 23:58:06 -0300

    feat: finish the nineteenth assignment

---
+++ Part 2/19HigherValDisplayer.py
@@ -0,0 +1,10 @@
+val1: float = 0
+val2: float = 0
+
+val1 = float(input("Insira o primeiro valor: "))
+val2 = float(input("Insira o segundo valor: "))
+
+if val1 > val2:
+    print("Maior valor:", val1)
+else:
+    print("Maior valor:", val2)
