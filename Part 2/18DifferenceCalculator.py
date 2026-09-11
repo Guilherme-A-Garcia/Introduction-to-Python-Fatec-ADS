4eb2bb5fb1609447e0b58bcf007e5ad6353386fd
Guilherme G. <guilherme.garcia.dev.br@gmail.com>
2026-09-10 23:46:37 -0300

    feat: finish the eighteenth assignment

---
+++ Part 2/18DifferenceCalculator.py
@@ -0,0 +1,13 @@
+int1: int = 0
+int2: int = 0
+int3: int = 0
+
+int1 = int(input("Insira o primeiro número: "))
+int2 = int(input("Insira o segundo número: "))
+
+if int1 > int2:
+    int3 = int1 - int2
+else:
+    int3 = in2 - int1
+
+print("Diferença: ", int3)
